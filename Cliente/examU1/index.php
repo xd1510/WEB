07d6a2c0344e3f2ef087fffe4632af1af5eff64c
xd1510 <davidbp1510@gmail.com>
2014-01-17 11:06:25 +0100

    examen
    U1

---
+++ Cliente/examU1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <input type="text" size="13" id="txt">
+        <script>
+            function escriuLog(){
+            var txt = document.getElementById("txt").innerHTM;
+                 var esplit = encriptado.split('-');
+                 if(split.length()==2){
+                    if(split[0] == "INC"){
+                        setInterval(esNavidadYNieva,3000);
+                    }
+                    else if(split[0]=="ALT"){
+                        
+                    }
+                 }
+                 else if(split.length()==3){
+                 
+                 }
+            }
+        </script>
+    </body>
+</html>
